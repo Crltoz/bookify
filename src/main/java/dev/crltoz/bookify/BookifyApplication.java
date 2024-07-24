7a722d8a7a156d88aae8374970b88e37e7325657
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-07-24 20:01:54 -0300

    add bookify application files

---
+++ src/main/java/dev/crltoz/bookify/BookifyApplication.java
@@ -0,0 +1,12 @@
+package dev.crltoz.bookify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookifyApplication.class, args);
+	}
+}
