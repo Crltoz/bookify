5c3a6d1644565b0c65dfeef710035391e157039e
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-07-27 17:34:35 -0300

    create delete product and dialog text components

---
+++ client/src/components/DeleteProduct.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function DeleteProduct({ product, confirmDelete, cancelDelete }) {
+  const handleConfirm = () => {
+    confirmDelete(product);
+  };
+
+  const handleClose = () => {
+    cancelDelete();
+  };
+
+  return (
+    <React.Fragment>
+      <Dialog
+        open={product != null}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Delete product confirmation."}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Are you sure you want to delete '{product?.name || "null"}' product?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} variant="contained">Cancelar</Button>
+          <Button onClick={handleConfirm} variant="contained" color="error">Borrar</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
