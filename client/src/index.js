b418d0800c9a684887c83a5c7dba39339b5569d1
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-07-27 17:35:15 -0300

    implement authorization for client

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+
+// get 'token' from localStorage
+const token = localStorage.getItem('token');
+// set 'Authorization' header
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
