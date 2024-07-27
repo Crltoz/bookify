f0e582748521279abe84a3c0c5c9f507deed603f
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-07-27 17:34:10 -0300

    add administracion component to app and remove some logs

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Admin from "./components/Admin";
 
 axios.defaults.baseURL = "http://localhost:8080/api";
 
@@ -11,20 +12,12 @@ axios.defaults.validateStatus = (status) => {
   return status >= 200 && status < 500;
 };
 
-axios.interceptors.response.use((response) => {
-  if (response.status === 401) {
-    console.log("Unauthorized");
-  }
-  return response;
-});
-
 function App() {
   const [products, setProducts] = useState();
   const getProducts = async () => {
     try {
       const response = await axios.get("/products/home");
       setProducts(response.data);
-      console.log(response.data);
     } catch (e) {
       console.error(e);
     }
@@ -40,6 +33,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home products={products} />} />
+          </Route>
+          <Route path="/administracion" element={<Layout />}>
+            <Route index element={<Admin />} />
           </Route>
         </Routes>
       </div>
