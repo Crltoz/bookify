0e9a60c74e6c44980ec3023f2863cc8cc8c539e5
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-07-28 19:16:13 -0300

    implement gallery in product info

--- client/src/components/ProductInfo.js
+++ client/src/components/ProductInfo.js
@@ -10,10 +10,15 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Slide from "@mui/material/Slide";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faCirclePlus,
+  faPhotoFilm,
+} from "@fortawesome/free-solid-svg-icons";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import "./css/ProductInfo.css";
+import DialogCarousel from "./DialogCarousel";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="right" ref={ref} {...props} />;
@@ -30,10 +35,17 @@ function srcset(image, width, height, rows = 1, cols = 1) {
 
 export default function ProductInfo({ product, onClose }) {
   const [images, setImages] = React.useState([]);
+  const [index, setIndex] = React.useState(-1);
+  const [showGalery, setShowGalery] = React.useState(false);
 
   React.useEffect(() => {
     setImages(product?.images || []);
   }, [product]);
+
+  const toggleGalery = (index) => {
+    setIndex(index);
+    setShowGalery(index !== -1);
+  };
 
   return (
     <React.Fragment>
@@ -43,6 +55,12 @@ export default function ProductInfo({ product, onClose }) {
         onClose={onClose}
         TransitionComponent={Transition}
       >
+        <DialogCarousel
+          open={showGalery}
+          images={images}
+          initialIndex={index}
+          handleClose={() => toggleGalery(-1)}
+        />
         <AppBar sx={{ position: "relative", bgcolor: "#3498DB" }}>
           <Toolbar>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
@@ -53,33 +71,47 @@ export default function ProductInfo({ product, onClose }) {
             </Button>
           </Toolbar>
         </AppBar>
-        <div className="d-flex p-4 justify-content-center gap-3 bg-secondary user-select-none">
-          <img className="rounded image-effect" src={images[0]} width="30%" height="95%" />
-          {images.length > 1 && (
-            <ImageList
-              sx={{
-                height: "100%",
-                width: "20%",
-                flexWrap: "nowrap",
-                transform: "translateZ(0)",
-                overflowX: "hidden",
-              }}
-              rowHeight={230}
-              gap={5}
-              variant="masonry"
-            >
-              {images.slice(1, 5).map((image) => (
-                <ImageListItem key={image} rows={1} cols={1}>
-                  <img
-                    className="rounded image-effect"
-                    {...srcset(image, 200, 150, 1, 1)}
-                    alt={product?.name}
-                    loading="lazy"
-                  />
-                </ImageListItem>
-              ))}
-            </ImageList>
-          )}
+        <div className="d-block p-4 bg-secondary">
+          <div className="d-flex justify-content-center align-items-center gap-3 user-select-none">
+            <img
+              className="rounded image-effect main-image"
+              src={images[0]}
+              onClick={() => toggleGalery(0)}
+            />
+            {images.length > 1 && (
+              <ImageList
+                sx={{
+                  height: "auto",
+                  width: "30%",
+                  flexWrap: "nowrap",
+                  transform: "translateZ(0)",
+                  overflowX: "hidden",
+                }}
+                rowHeight={230}
+                gap={5}
+                variant="masonry"
+                className="d-none d-lg-block"
+              >
+                {images.slice(1, 5).map((image) => (
+                  <ImageListItem key={image} rows={1} cols={1}>
+                    <img
+                      className="rounded image-effect"
+                      {...srcset(image, 200, 150, 1, 1)}
+                      alt={product?.name}
+                      loading="lazy"
+                      onClick={() => toggleGalery(images.indexOf(image))}
+                    />
+                  </ImageListItem>
+                ))}
+              </ImageList>
+            )}
+          </div>
+          <div className="d-flex justify-content-end mt-2">
+            <button className="btn btn-outline-primary" onClick={() => toggleGalery(0)}>
+              <FontAwesomeIcon icon={faCirclePlus} className="me-2" />
+              <FontAwesomeIcon icon={faPhotoFilm} className="me-2" />
+            </button>
+          </div>
         </div>
         <List>
           <ListItemButton
