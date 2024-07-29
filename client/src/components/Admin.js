d0f841d0b0aa8567fd1b0f85e6942ee251f5e301
Crltoz <43712486+Crltoz@users.noreply.github.com>
2024-07-29 18:44:55 -0300

    add category editor in admin panel

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -5,6 +5,7 @@ import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 import AdminListProducts from "./AdminListProducts";
 import EditProduct from "./EditProduct";
 import DialogText from "./DialogText";
+import EditCategory from "./EditCategory";
 
 const Admin = () => {
   const [validToken, setValidToken] = useState(null);
@@ -14,6 +15,7 @@ const Admin = () => {
   const [showProducts, setShowProducts] = useState(false);
   const [createProduct, setCreateProduct] = useState(false);
   const [dialogText, setDialogText] = useState("");
+  const [createCategory, setCreateCategory] = useState(false);
 
   const token = localStorage.getItem("token");
 
@@ -71,6 +73,10 @@ const Admin = () => {
     setCreateProduct(true);
   };
 
+  const enableCreateCategory = () => {
+    setCreateCategory(true);
+  };
+
   const confirmCreateProduct = async (product) => {
     try {
       const updateListProduct = showProducts;
@@ -106,6 +112,39 @@ const Admin = () => {
       console.error(error);
       setDialogText("Error al crear el producto");
       setCreateProduct(false);
+    }
+  };
+
+  const confirmCreateCategory = async (category) => {
+    try {
+      const response = await axios.post("/categories/add", category);
+      switch (response.status) {
+        case 201: {
+          setCreateCategory(false);
+          setDialogText("Categoría creada correctamente");
+          break;
+        }
+        case 409: {
+          setDialogText(
+            "Ya existe una categoría con ese nombre. Intenta con otro."
+          );
+          break;
+        }
+        case 401: {
+          setDialogText("No tienes permisos para crear categorías");
+          setCreateCategory(false);
+          break;
+        }
+        default: {
+          setDialogText("Error al crear la categoría");
+          setCreateCategory(false);
+          break;
+        }
+      }
+    } catch (error) {
+      console.error(error);
+      setDialogText("Error al crear la categoría");
+      setCreateCategory(false);
     }
   };
 
@@ -145,6 +184,14 @@ const Admin = () => {
                 <span>Crea un nuevo producto y publicalo en la web.</span>
               </div>
             </div>
+            <div className="row">
+              <div className="col-12 col-lg-6">
+                <button className="btn btn-primary m-4" onClick={enableCreateCategory}>Agregar categoría</button>
+              </div>
+              <div className="col d-flex justify-content-center align-items-center">
+                <span>Crea una nueva categoría que podrá ser asignada a un producto.</span>
+              </div>
+            </div>
           </div>
 
           {showProducts && <AdminListProducts />}
@@ -153,6 +200,12 @@ const Admin = () => {
             product={{}}
             onConfirm={confirmCreateProduct}
             onCancel={() => setCreateProduct(false)}
+          />
+          <EditCategory
+            open={createCategory}
+            category={{}}
+            onConfirm={confirmCreateCategory}
+            onCancel={() => setCreateCategory(false)}
           />
         </div>
       ) : (
